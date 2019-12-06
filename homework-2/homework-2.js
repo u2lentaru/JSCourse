37d8ca771a14b2d27f73a2b826ddc9c0f14d8d7b
u2 <u2@lenta.ru>
2019-12-06 21:36:17 +0500

    Task 8 done

--- homework-2/homework-2.js
+++ homework-2/homework-2.js
@@ -47,3 +47,10 @@
 
 //### 8. *С помощью рекурсии организовать функцию возведения числа в степень. 
 //Формат: function power(val, pow), где val – заданное число, pow – степень.
+
+function power(val,pow){
+    if pow==1{
+        return val;
+    }
+    return val*power(val,pow-1);
+}
