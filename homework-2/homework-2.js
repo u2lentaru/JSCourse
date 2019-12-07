6e293bf03cffe1d95ef1bafb63fffef48673fc6f
u2 <u2@lenta.ru>
2019-12-07 14:36:17 +0500

    Task 8 hbm

--- homework-2/homework-2.js
+++ homework-2/homework-2.js
@@ -121,7 +121,7 @@ alert(null == 0);
 //Формат: function power(val, pow), где val – заданное число, pow – степень.
 
 function power(val,pow){
-    if pow==1{
+    if (pow==1){
         return val;
     }
     return val*power(val,pow-1);
