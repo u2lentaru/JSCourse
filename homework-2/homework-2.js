666c52ebd0ae4c12abb2c0f14fe6a6f37e1fee0b
u2 <u2@lenta.ru>
2019-12-07 12:51:01 +0500

    Task 4 done

--- homework-2/homework-2.js
+++ homework-2/homework-2.js
@@ -38,7 +38,39 @@ if (a>=0 && b>=0) || (a<0 && b<0){
 //4. Присвоить переменной а значение в промежутке [0..15]. С помощью оператора switch организовать 
 //вывод чисел от a до 15. 
 
-parse int(Math.random()*(14-1)+1);
+var a=parse int(Math.random()*(15-1)+1);
+switch(a){
+    case 1:
+        alert(a);
+    case 2:
+        alert(a);    
+    case 3:
+        alert(a);
+    case 4:
+        alert(a);
+    case 5:
+        alert(a);
+    case 6:
+        alert(a);
+    case 7:
+        alert(a);
+    case 8:
+        alert(a);
+    case 9:
+        alert(a);
+    case 10:
+        alert(a);
+    case 11:
+        alert(a);
+    case 12:
+        alert(a);
+    case 13:
+        alert(a);
+    case 14:
+        alert(a);
+    case 15:
+        alert(a);
+}
 
 //### 5. Реализовать основные 4 арифметические операции в виде функций с двумя параметрами. 
 //Обязательно использовать оператор return. 
