abc46818d300e9cf33166628d6b9cc549691e7c7
u2 <u2@lenta.ru>
2019-12-18 12:26:27 +0500

    Task 1 has been modified

--- homework-5/homework-5.js
+++ homework-5/homework-5.js
@@ -2,34 +2,78 @@
 //своему желанию. Доска должна быть разлинована соответствующим образом, т.е. чередовать черные и 
 //белые ячейки. Строки должны нумероваться числами от 1 до 8, столбцы – латинскими буквами A, B, C, D, E, F, G, H.
 
-const cells = [];
-for(var i=0;i<64;i++){
-  //document.write(i);
-  cells.push({
-    color: i%2?'black':'white',
-    x: i%8,
-    y: ~~(i/8)//floor?
-  });
+function drawChessBoard(){
+  var table = document.createElement('table');
+  var flag = true;
+  
+  var trr = document.createElement('tr');
+  var arr = [' ','a','b','c','d','e','f','g','h'];
+  
+  for(var k = 0; k<9; k++){
+      
+      var td = document.createElement('td');
+      td.style.width='50px';
+      td.style.height='50px';
+      td.style.background='white';
+      td.innerHTML=arr[k];
+      td.style.transform='rotate(180deg)';
+      
+      trr.appendChild(td);
+      
+  }
+  table.appendChild(trr);
+  
+  //генерим ячейки
+  for(var i = 0; i<8; i++){
+      var tr = document.createElement('tr');
+      
+      //создаем ячейку для цифр
+      var tdd = document.createElement('td');
+      tdd.style.width='50px';
+      tdd.style.height='50px';
+      tdd.innerHTML=8-i;
+      tr.appendChild(tdd);
+      
+      for(var j=0; j<8; j++){
+          
+          if(j==0)
+              flag=!flag;
+          
+          var td = document.createElement('td');
+          
+          td.style.width='50px';
+          td.style.height='50px';
+          if(flag){
+              td.style.background='brown';    
+          }else
+              td.style.background='yellow';    
+              
+          tr.appendChild(td);
+          flag=!flag;
+      }
+      table.appendChild(tr);
+  }
+  
+  //генерим буквы
+  trr = document.createElement('tr');
+  for(var k = 0; k<9; k++){
+      
+      var td = document.createElement('td');
+      td.style.width='50px';
+      td.style.height='50px';
+      td.style.background='white';
+      td.innerHTML=arr[k];
+      //td.style.transform='rotate(180deg)';
+      
+      trr.appendChild(td);
+      
+  }
+  table.appendChild(trr);
+  
+  document.body.appendChild(table);
 }
-var mbd = document.getElementsByTagName("body");
-var node;
-    document.write(cells.length);
-for (var i in cells) {
-    node = document.createElement("div");
-    //document.write("create");
-    //mbd.appendChild(node);
-    node.style.width = '30px';
-    node.style.height = '30px';
-    //node.style.backgroundColor = 'black';
-    //node.innerHTML('<p>kjh</p>');
-    node.style.backgroundColor = cells[i].color;
-    //node.style.float = 'left';
-    node.left = cells[i].x*30+'px';
-    node.top = cells[i].y*30+'px';
-    //list1.appendChild(node);
-    mbd[0].appendChild(node);
-    //document.write("append");
-}
+
+drawChessBoard();
 
 //2. Заполнить созданную таблицу буквами, отвечающими за шахматную фигуру, например К – король, 
 //Ф – ферзь и т.п., причем все фигуры должны стоять на своих местах и быть соответственно черными и белыми.
