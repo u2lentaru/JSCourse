d03fb63b5583cee638799c561d34743f3727590c
u2 <u2@lenta.ru>
2019-12-22 14:41:22 +0500

    Tash 2 HTML added

--- homework-6/homework-6.js
+++ homework-6/homework-6.js
@@ -49,8 +49,29 @@ function showBigPicture(){
 //при нажатии на которую происходит добавление имени и цены товара в блок корзины. Корзина должна уметь 
 //считать общую сумму заказа.
 
+//HTML
+/*<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<div id="bPict">Big Picture</div>
+<div>
+    <img src="./img/small/1.jpg" width="50" height="30" alt="img_1" id="img_1">
+    <img src="./img/small/2.jpg" width="50" height="30" alt="img_2" id="img_2">
+    <img src="./img/small/3.jpg" width="50" height="30" alt="img_3" id="img_3">
+</div>
+<script src="hw_6_2.js">
+</script>
+</body>
+</html>*/
+
+//hw_6_2.js
 
 
 //3) *Добавить в галерею функцию перехода к следующему изображению. По сторонам от большой картинки должны 
 //быть стрелки «вперед» и «назад», по нажатию на которые происходит замена изображения на следующее или 
 //предыдущее.
+
