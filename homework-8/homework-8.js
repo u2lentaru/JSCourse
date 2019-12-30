928b7bf255f0320998955e54c02a86ff36e17467
u2 <u2@lenta.ru>
2019-12-30 14:11:59 +0500

    hw-8.js hbm

--- homework-8/homework-8.js
+++ homework-8/homework-8.js
@@ -14,20 +14,32 @@ function currentScore(){
 //    var a = 1;
 //}
 //alert(a);
+
+
+
 //var b = function a(x) {
 //    x && a(--x);
 //};
 //alert(a);
+
+
+
 //function a(x) {
 //    return x * 2;
 //}
 //var a;
 //alert(a);
+
+
+
 //function b(x, y, a) {
 //    arguments[2] = 10;
 //    alert(a);
 //}
 //b(1, 2, 3);
+
+
+
 //function a() {
 //    alert(this);
 //}
