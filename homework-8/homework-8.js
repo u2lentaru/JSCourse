8b04fc4f21f8fd7ed9eb345948302895b08cfc67
u2 <u2@lenta.ru>
2019-12-30 17:07:59 +0500

    hw-8.js hbm

--- homework-8/homework-8.js
+++ homework-8/homework-8.js
@@ -12,39 +12,39 @@ function currentScore(){
 
 
 //2. Не выполняя кода, ответить, что выведет браузер и почему:
-//if (!("a" in window)) {
-//    var a = 1;
-//}
-//alert(a);
+if (!("a" in window)) {
+    var a = 1;
+}
+alert(a);
 
 //1 - "a" in window даёт false => a объявляется со значением 1
 
-//var b = function a(x) {
-//    x && a(--x);
-//};
-//alert(a);
+var b = function a(x) {
+    x && a(--x);
+};
+alert(a);
 
+//ничего не выведет, т. к. функции не передаётся никакое значение
 
-
-//function a(x) {
-//    return x * 2;
-//}
-//var a;
-//alert(a);
+function a(x) {
+    return x * 2;
+}
+var a;
+alert(a);
 
 //
 
-//function b(x, y, a) {
-//    arguments[2] = 10;
-//    alert(a);
-//}
-//b(1, 2, 3);
+function b(x, y, a) {
+    arguments[2] = 10;
+    alert(a);
+}
+b(1, 2, 3);
 
 //10 так как arguments[2] присвоено значение 10.
 
-//function a() {
-//    alert(this);
-//}
-//a.call(null);
+function a() {
+    alert(this);
+}
+a.call(null);
 
 //объект window, так как this имеет значение null.
